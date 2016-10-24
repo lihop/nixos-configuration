36d1e388d7e389474160285f667245347955ddfc
Leroy Hopson <git@leroy.geek.nz>
2016-10-24 22:08:15 +1300

    Add namespace for roles

--- release.nix
+++ release.nix
@@ -13,18 +13,20 @@ let
   } args);
 in
 {
-  common = roleTest /common { };
-  gaming = roleTest /gaming { };
-  hydra = roleTest /hydra { };
-  hydraProxy = roleTest /hydra/proxy.nix { };
-  hydraSlave = roleTest /hydra/slave.nix { };
-  mail = roleTest /mail { };
-  monitored = roleTest /monitored { };
-  mysqlMaster = roleTest /mysql/master.nix { };
-  mysqlSlave = roleTest /mysql/slave.nix { };
-  server = roleTest /server { };
-  workstation = roleTest /workstation { };
-  workstationExtra = roleTest /workstation-extra { };
+  roles = {
+    common = roleTest /common { };
+    gaming = roleTest /gaming { };
+    hydra = roleTest /hydra { };
+    hydraProxy = roleTest /hydra/proxy.nix { };
+    hydraSlave = roleTest /hydra/slave.nix { };
+    mail = roleTest /mail { };
+    monitored = roleTest /monitored { };
+    mysqlMaster = roleTest /mysql/master.nix { };
+    mysqlSlave = roleTest /mysql/slave.nix { };
+    server = roleTest /server { };
+    workstation = roleTest /workstation { };
+    workstationExtra = roleTest /workstation-extra { };
+  };
 
   environments = pkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ] (system:
     let
